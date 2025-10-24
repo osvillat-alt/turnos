79dfb2119bd7ba97272534a1c62976046c398a76
osvillat-alt <osvillat@gmail.com>
2025-10-24 14:41:23 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "turnos-cache-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./rol.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/cemex_logo.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
